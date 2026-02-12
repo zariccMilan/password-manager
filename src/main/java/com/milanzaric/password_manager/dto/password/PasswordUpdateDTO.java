f0ee6494f4f7f5adc384401ef36af2ab413edd18
MilanZarić <zaric.milan11@gmail.com>
2026-02-12 12:15:39 +0100

    Added validations in PasswordDTOs

--- src/main/java/com/milanzaric/password_manager/dto/password/PasswordUpdateDTO.java
+++ src/main/java/com/milanzaric/password_manager/dto/password/PasswordUpdateDTO.java
@@ -1,6 +1,9 @@
 package com.milanzaric.password_manager.dto.password;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,11 @@ import java.util.UUID;
 @AllArgsConstructor
 public class PasswordUpdateDTO {
 
+
+    @NotBlank
+    @Size(max = 36)
     private String passwordHash;
+
+    @NotNull
     private UUID userId;
 }
