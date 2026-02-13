e81a317ff7839010d7ed95a71fe3e47b05000736
MilanZarić <zaric.milan11@gmail.com>
2026-02-13 16:11:53 +0100

    Added PasswordDtoMapper

---
+++ src/main/java/com/milanzaric/password_manager/mapping/PasswordDtoMapper.java
@@ -0,0 +1,15 @@
+package com.milanzaric.password_manager.mapping;
+
+
+import com.milanzaric.password_manager.dto.password.PasswordDTO;
+import com.milanzaric.password_manager.model.Password;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PasswordDtoMapper {
+
+    PasswordDTO mapToPasswordDTO(Password password);
+    List<PasswordDTO> mapToListPasswordDTO(List<Password> passwords);
+}
