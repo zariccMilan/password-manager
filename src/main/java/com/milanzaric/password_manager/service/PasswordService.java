f932ccf266aa11efc3e0d2f4291a8ec0e28a4841
MilanZarić <zaric.milan11@gmail.com>
2026-02-16 02:08:26 +0100

    PasswordService interface Only for one method for testing

---
+++ src/main/java/com/milanzaric/password_manager/service/PasswordService.java
@@ -0,0 +1,13 @@
+package com.milanzaric.password_manager.service;
+
+import com.milanzaric.password_manager.dto.password.PasswordDTO;
+
+import java.util.List;
+
+public interface PasswordService {
+
+    List<PasswordDTO> getAllPasswords();
+}
+
+
+// 019c57b9-7d8d-749b-982f-88ece02176c4 // password
