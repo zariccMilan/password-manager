46219a0a25709599eb8067cc4147d265e5567bf1
MilanZarić <zaric.milan11@gmail.com>
2026-02-16 15:15:47 +0100

    Added Optional in PasswordMapper

--- src/main/java/com/milanzaric/password_manager/repository/mapper/PasswordMapper.java
+++ src/main/java/com/milanzaric/password_manager/repository/mapper/PasswordMapper.java
@@ -5,13 +5,14 @@ import com.milanzaric.password_manager.model.Password;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Mapper
 public interface PasswordMapper {
 
     List<Password> findAll();
-    Password findById(UUID id);
+    Optional<Password> findById(UUID id);
     void insert(Password password);
     void update(Password password);
     void delete(UUID id);
